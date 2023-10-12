1121396d7ccd856ef26795eb1ce6550ff95cfa98
Julio <ulitoo@outlook.com>
2023-10-12 03:07:01 -0700

    Create temporal test commit

--- Basics/0.HelloW.cpp
+++ Basics/0.HelloW.cpp
@@ -8,7 +8,7 @@ using namespace std;
 int main()
 {
     int iteration = 5;
-    vector<string> msg{"Hello", "C++", "World", "from", "VS Code", "and the C++ extension!"};
+    vector<string> msg{"Hello", "C++", "World", "from", "VS Code", "and the C++ extension!!!"};
     for (const string &word : msg)
     {
         cout << word << " "; // << iteration*iteration <<"  ";
