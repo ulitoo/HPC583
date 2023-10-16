3e40d4bed2c665b37720b4fdf89dca6bda2f5aba
Julio <ulitoo@outlook.com>
2023-10-16 01:03:18 -0700

    Julio upload domingo cansado

--- Basics/3.TestMPI.cpp
+++ Basics/3.TestMPI.cpp
@@ -5,13 +5,25 @@ using namespace std;
 int main(int argc, char *argv[])
 {
     char processor_name[MPI_MAX_PROCESSOR_NAME];
-    int size, rank, len;
+    int size, rank, len, tag;
+    MPI_Status status;
     MPI_Init(&argc, &argv);
     MPI_Comm_size(MPI_COMM_WORLD, &size);
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
     MPI_Get_processor_name(processor_name, &len);
-    cout << "Hello world from process " << rank << " of " << size << " on " << processor_name << endl;
-    //cout << "Length " << argv[4] << endl;
+    
+    if (rank == 3) {
+        int message = 4222;
+        MPI_Send(&message,1,MPI_INT,1,tag,MPI_COMM_WORLD);
+        std::cout<<"Process 3 Sent Message: "<< message<<std::endl;
+    }
+    else if (rank == 1) {
+        int message;
+        MPI_Recv(&message,1,MPI_INT,3,tag,MPI_COMM_WORLD,&status);
+        std::cout<<"Process 1 Received Message: "<< message<<std::endl;
+    }
+    
+    //cout << "Hello world from process " << rank << " of " << size << " on " << processor_name << endl;
     MPI_Finalize();
     return 0;
 }
