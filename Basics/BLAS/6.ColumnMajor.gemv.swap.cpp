9e2fc9319b959332c73f205534509dad5ac495a1
Julio <ulitoo@outlook.com>
2023-11-11 19:50:12 -0800

    swap and gemv

--- Basics/BLAS/6.ColumnMajor.gemv.swap.cpp
+++ Basics/BLAS/6.ColumnMajor.gemv.swap.cpp
@@ -1,66 +1,200 @@
-#include <random>
 #include <cblas.h>
 #include <iostream>
+#include <complex>
 #include <vector>
 #include <cmath>
-#include <string>
 #include <fstream>
+#include "gnuplot-iostream.h" // Gnuplot C++ interface
+
 
 using namespace std;
 
 // 	Problem , get a matrix from disk into Column Major Order
-// Compute the decompositions depicted in linear algebra
+//  Implement GEMV
+//  Implement Col Swap and Row Swap 
+//  Compare with GEMV BLAS performance
+
+/////////////////////////////     FUNCTIONS
+
+void PrintColMatrix(std::vector<double> matrix)
+{
+    int n = round(sqrt(matrix.size()));
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            cout << matrix[j*n+i] << " ";
+        }
+        cout << "\n";
+    }
+}
+
+void PrintVector(int n, double *vector)
+{
+    for (int i = 0; i < n; i++)
+    {
+        cout << vector[i] << " ";
+    }
+    cout << "\n";
+}
+
+void InitVector(int n, double *vector)
+{
+    for (int i = 0; i < n; i++)
+    {
+        vector[i] = (double)i;
+    }
+}
+
+void gemv1(int n, double alpha, std::vector<double> matrix_A, double *vector_x, double beta, double *vector_y, double *vector_z)
+{
+    for (int i = 0; i < n; i++)
+    {
+        vector_z[i] = beta * vector_y[i];
+        for (int j = 0; j < n; j++)
+        {
+            vector_z[i] += alpha * matrix_A[i+(n*j)] * vector_x[j];
+        }  
+    }
+}
+
+void gemv2(int n, double alpha, std::vector<double> matrix_A_trsps, double *vector_x, double beta, double *vector_y, double *vector_z)
+{
+    for (int i = 0; i < n; i++)
+    {
+        vector_z[i] = beta * vector_y[i];
+        for (int j = 0; j < n; j++)
+        {
+            vector_z[i] += alpha * matrix_A_trsps[j+(n*i)] * vector_x[j];
+        }  
+    }
+}
+
+std::vector<double> SwapCol_ColMajMatrix(std::vector<double> matrix,int from, int towards)
+{
+    int n = round(sqrt(matrix.size()));
+    double tmpval;
+    for (int i = 0; i < n; i++)
+    {
+        tmpval = matrix[towards*n+i];
+        matrix[towards*n+i]= matrix[from*n+i];
+        matrix[from*n+i]=tmpval;
+    }
+    return matrix;
+}
+
+std::vector<double> SwapRow_ColMajMatrix(std::vector<double> matrix,int from, int towards)
+{
+    int n = round(sqrt(matrix.size()));
+    double tmpval;
+    for (int i = 0; i < n; i++)
+    {
+        tmpval = matrix[towards+i*n];
+        matrix[towards+i*n]= matrix[from+i*n];
+        matrix[from+i*n]=tmpval;
+    }
+    return matrix;
+}
+
+std::vector<double> RowtoColMajor_Transpose(std::vector<double> matrix)
+{
+    int n = round(sqrt(matrix.size()));
+    std::vector<double> tmpmatrixCol(n * n);
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            tmpmatrixCol[j+(i*n)] = matrix[i+(j*n)];
+        }
+    }
+    return tmpmatrixCol;
+}
+
+
+/////////////////////////////     MAIN
 
 int main ( int argc, char* argv[] ) {
+    // Timers
+    auto start = std::chrono::high_resolution_clock::now();
+    auto stop = std::chrono::high_resolution_clock::now();
+    auto duration = std::chrono::duration_cast<std::chrono::nanoseconds>(stop - start);
+    long double elapsed_time;
 
-    if (argc != 3)
+    if (argc != 5)
     {
-        std::cerr << "Usage: " << argv[0] << " <filename> rank(A)" << std::endl;
+        std::cerr << "Usage: " << argv[0] << " <filename> rank(A) beta alpha" << std::endl;
         return 1;
     }
 
     const int n = std::atoi(argv[2]); // rank
+    double alpha = (double)std::atoi(argv[3]);
+    double beta = (double)std::atoi(argv[4]);
 
-    // Open the binary file for reading
+    // Create a vector to store the Column Major matrix data
+    std::vector<double> matrixCol(n * n);
+    // Create and init the rest of the vectors for the GEMV 
+    double vectorx[n] = {1.0};
+    double vectory[n] = {1.0};
+    double vectorz[n] = {0.0};
+    InitVector(n, vectorx);
+    InitVector(n, vectory);
+
+    // Open the binary file for reading and handle error
     std::ifstream input(argv[1], std::ios::binary);
-    if (!input)
-    {
-        std::cerr << "Error: could not open file for reading" << std::endl;
-        return 1;
-    }
-
-    // Create a vector to store the matrix data
-    std::vector<double> matrix(n * n);
+    if (!input){std::cerr << "Error: could not open file for reading" << std::endl; return 1;}
 
     // Read the binary data into the vector
-    input.read(reinterpret_cast<char *>(matrix.data()), sizeof(double) * matrix.size());
+    input.read(reinterpret_cast<char *>(matrixCol.data()), sizeof(double) * matrixCol.size());
 
-    // Check if read was successful
-    if (!input)
-    {
-        std::cerr << "Error: could not read file" << std::endl;
-        return 1;
-    }
+    // Check if read was successful and handle error 
+    if (!input) {std::cerr << "Error: could not read file" << std::endl; return 1;}
 
     // Print the matrix elements
-    cout << "This is the Matrix in Row Major Order \n";
-    for (int i = 0; i < n; ++i)
-    {
-        for (int j = 0; j < n; ++j)
-        {
-            std::cout << matrix[i * n + j] << " ";
-        }
-        std::cout << "\n";
-    }
-    cout << "\nThis is the Matrix in Column Major Order \n";
-    for (int i = 0; i < n; ++i)
-    {
-        for (int j = 0; j < n; ++j)
-        {
-            std::cout << matrix[j * n + i] << " ";
-        }
-        std::cout << "\n";
-    }
+    cout << "\nColMajor matrix Print:\n";
+    PrintColMatrix(matrixCol);
+    cout << "\nResult VectorX Print:\n";
+    PrintVector(n, vectorx);
+    cout << "\nResult VectorY Print:\n";
+    PrintVector(n, vectory);
+    cout << "\nResult VectorZ Print:\n";
+    PrintVector(n, vectorz);
+    
+    
+/*
+    start = std::chrono::high_resolution_clock::now();
+    gemv1(n,alpha,matrixCol,vectorx,beta,vectory,vectorz);
+    stop = std::chrono::high_resolution_clock::now();
+    duration = std::chrono::duration_cast<std::chrono::nanoseconds>(stop - start);
+    elapsed_time = (duration.count() * 1.e-9);
+    std::cout << "\nNaive Elapsed Time: " << elapsed_time << " s.\n";
+
+  
+    std::vector<double> matrixRow(n * n);
+    matrixRow = RowtoColMajor_Transpose(matrixCol);
+    start = std::chrono::high_resolution_clock::now();
+    gemv2(n,alpha,matrixRow,vectorx,beta,vectory,vectorz);
+    stop = std::chrono::high_resolution_clock::now();
+    duration = std::chrono::duration_cast<std::chrono::nanoseconds>(stop - start);
+    elapsed_time = (duration.count() * 1.e-9);
+    std::cout << "\nTransposed Elapsed Time: " << elapsed_time << " s.\n";
+*/
+
+    //Compare with BLAS
+    start = std::chrono::high_resolution_clock::now();
+    cblas_dgemv(CblasColMajor, CblasNoTrans, n, n, alpha, &matrixCol[0], n, vectorx, 1, beta, vectory, 1);
+    cout << "\nResult VectorZ Print con CBLAS:\n";
+    PrintVector(n, vectory);
+    stop = std::chrono::high_resolution_clock::now();
+    duration = std::chrono::duration_cast<std::chrono::nanoseconds>(stop - start);
+    elapsed_time = (duration.count() * 1.e-9);
+    std::cout << "\nCBLAS Elapsed Time: " << elapsed_time << " s.\n";
+
+
+    cout << "\nColMajor matrix after swap Col alpha with beta Print:\n";
+    PrintColMatrix(SwapCol_ColMajMatrix(matrixCol,round(alpha),round(beta)));
+    
+    cout << "\nColMajor matrix after swap Row alpha with beta Print:\n";
+    PrintColMatrix(SwapRow_ColMajMatrix(matrixCol,round(alpha),round(beta)));    
 
     return 0;
 }
--- Basics/BLAS/6.WriteColumnMajorOrder.cpp
+++ Basics/BLAS/6.WriteColumnMajorOrder.cpp
@@ -1,9 +1,49 @@
 #include <iostream>
 #include <fstream>
 #include <vector>
-#include "6.WriteColumnMajorOrder_f.h"
+#include <cmath>
+// in case you want to include all the functions in a sigle file
+//#include "6.WriteColumnMajorOrder_f.h"
 
 using namespace std;
+
+void PrintRowMatrix(std::vector<double> matrix)
+{
+    int n = round(sqrt(matrix.size()));
+    for (int i = 0; i < n * n; i++)
+    {
+        cout << matrix[i] << " ";
+        if (i%n==n-1) {
+            cout << "\n";
+        }
+    }  
+}
+void PrintColMatrix(std::vector<double> matrix)
+{
+    int n = round(sqrt(matrix.size()));
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            cout << matrix[j*n+i] << " ";
+        }
+        cout << "\n";
+    }
+}
+
+std::vector<double> RowtoColMajor_Transpose(std::vector<double> matrix)
+{
+    int n = round(sqrt(matrix.size()));
+    std::vector<double> tmpmatrixCol(n * n);
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            tmpmatrixCol[j+(i*n)] = matrix[i+(j*n)];
+        }
+    }
+    return tmpmatrixCol;
+}
 
 int main(int argc, char *argv[])
 {
@@ -15,32 +55,17 @@ int main(int argc, char *argv[])
 
     const int n = std::atoi(argv[1]);
     
-    // Create the matrix and fill it with Ordered N values
-    std::vector<double> matrix(n * n);
-    cout << "This is the Matrix in Row Major Order \n";
-    for (int i = 0; i < n * n; i++)
-    {
-        matrix[i] = (double)i;
-        cout << matrix[i] << " ";
-        if (i%n==n-1) {
-            cout << "\n";
-        }
-    }
-    cout << "\nThis is the Matrix in Column Major Order \n";
-    for (int i = 0; i < n; i++)
-    {
-        for (int j = 0; j < n; j++)
-        {
-            cout << matrix[j*n+i] << " ";
-        }
-        cout << "\n";
-    }
+    // Create the matrix IN ROW MAJOR and fill it with Ordered N values
+    std::vector<double> matrixRow(n * n);
+    std::vector<double> matrixCol(n * n);
+    for (int i = 0; i < n * n; i++) {matrixRow[i] = (double)i;}
+    matrixCol = RowtoColMajor_Transpose(matrixRow);
 
-    // Write the matrix to file in binary format
-    std::ofstream outfile("matrix.bin", std::ios::out | std::ios::binary);
+    // Write the Column Major matrix to file in binary format
+    std::ofstream outfile("matrixCol.bin", std::ios::out | std::ios::binary);
     if (outfile.is_open())
     {
-        outfile.write(reinterpret_cast<const char *>(matrix.data()), sizeof(double) * n * n);
+        outfile.write(reinterpret_cast<const char *>(matrixCol.data()), sizeof(double) * n * n);
         outfile.close();
     }
     else
@@ -48,8 +73,14 @@ int main(int argc, char *argv[])
         std::cerr << "Failed to open file: matrix.bin" << std::endl;
         return 1;
     }
+    
+    // Print Matrix in Row Major and Colum Major
+    cout << "\nRowMajor matrix Print:\n";
+    //PrintRowMatrix(matrixRow);
+    cout << "\nColMajor matrix Print:\n";
+    //PrintColMatrix(matrixCol);
 
-    cout << "\nMatrix correctly Created\n\n";
+    cout << "\nMatrix correctly Created and written in file as ColMajor\n\n";
 
     return 0;
 }
