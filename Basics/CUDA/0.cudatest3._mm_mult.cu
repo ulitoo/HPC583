8b9b049f83d0b3da85ebcadb9d1a24fac5096be0
Julio <ulitoo@outlook.com>
2024-02-08 20:53:07 -0800

    .

--- Basics/CUDA/0.cudatest3._mm_mult.cu
+++ Basics/CUDA/0.cudatest3._mm_mult.cu
@@ -1,14 +1,15 @@
 #include <iostream>
 
 // Matrix multiplication kernel
-__global__ void matrixMultiply(int *a, int *b, int *c, int N)
+__global__ void matrixMultiply(double *a, double *b, double *c, int N)
 {
     int row = blockIdx.y * blockDim.y + threadIdx.y;
     int col = blockIdx.x * blockDim.x + threadIdx.x;
-
+    //printf("N is:%d , thread x: %d and thread y: %d \n",N,threadIdx.x,threadIdx.y);
+    //printf("N is:%d , col %d and row: %d \n",N,col,row);
     if (row < N && col < N)
     {
-        int sum = 0;
+        double sum = 0;
         for (int i = 0; i < N; ++i)
         {
             sum += a[row * N + i] * b[i * N + col];
@@ -17,38 +18,58 @@ __global__ void matrixMultiply(int *a, int *b, int *c, int N)
     }
 }
 
-int main( int argc, char* argv[] )
+void matrixPrint(double *a, int N)
 {
-    
+    for (int i = 0; i < N; ++i)
+    {
+        for (int j = 0; j < N; ++j)
+        {
+            std::cout << a[i * N + j] << "\t";
+        }
+        std::cout << std::endl;
+    }
+    std::cout << std::endl;
+}
+
+int getMaxBlockSize()
+{
+    int blockSize, minGridSize;
+    cudaOccupancyMaxPotentialBlockSize(&minGridSize, &blockSize, matrixMultiply, 0, 0);
+    return blockSize;
+}
+
+
+int main(int argc, char *argv[])
+{
+
     if (argc != 2)
     {
         std::cerr << "Usage: " << argv[0] << " n (Dimension of Matrix)" << std::endl;
         return 1;
     }
 
-    const int N = std::atoi(argv[1]);  // Matrix size (N x N)
-
+    const int N = std::atoi(argv[1]); // Matrix size (N x N)
 
     // Matrix dimensions
-    const int matrixSize = N * N * sizeof(int);
+    const int matrixSize = N * N * sizeof(double);
 
     // Host matrices
-    int *h_A, *h_B, *h_C;
+    double *h_A, *h_B, *h_C;
 
     // Allocate memory for host matrices
-    h_A = new int[N * N];
-    h_B = new int[N * N];
-    h_C = new int[N * N];
+    h_A = new double[N * N];
+    h_B = new double[N * N];
+    h_C = new double[N * N];
 
     // Initialize host matrices
     for (int i = 0; i < N * N; ++i)
     {
-        h_A[i] = i;
-        h_B[i] = i;
+        h_A[i] = (double)i+(double)1.0/7.0;
+        h_B[i] = (double)i-(double)1.0/3.0;
     }
 
     // Allocate memory on the device (GPU)
-    int *d_A, *d_B, *d_C;
+    double *d_A, *d_B, *d_C;
     cudaMalloc((void **)&d_A, matrixSize);
     cudaMalloc((void **)&d_B, matrixSize);
     cudaMalloc((void **)&d_C, matrixSize);
@@ -56,10 +77,19 @@ int main( int argc, char* argv[] )
     // Copy host matrices to device
     cudaMemcpy(d_A, h_A, matrixSize, cudaMemcpyHostToDevice);
     cudaMemcpy(d_B, h_B, matrixSize, cudaMemcpyHostToDevice);
+    
+    int maxBL1D = getMaxBlockSize();
+    int maxBL2D = static_cast<int>(sqrt(maxBL1D));
+
+    std::cout<< "1D MAXBLOCKSIZE: " << maxBL1D << "\n";
+    std::cout<< "2D MAXBLOCKSIZE , each Dimension Max: " << maxBL2D << "\n";
 
     // Define grid and block dimensions
-    dim3 blockSize(2, 2);
+    //dim3 blockSize(32,32);
+
+    dim3 blockSize(maxBL2D,maxBL2D);
     dim3 gridSize((N + blockSize.x - 1) / blockSize.x, (N + blockSize.y - 1) / blockSize.y);
+
 
     // Launch matrix multiplication kernel
     matrixMultiply<<<gridSize, blockSize>>>(d_A, d_B, d_C, N);
@@ -67,38 +97,13 @@ int main( int argc, char* argv[] )
     // Copy result matrix from device to host
     cudaMemcpy(h_C, d_C, matrixSize, cudaMemcpyDeviceToHost);
 
-    // Print the a matrix
+    // Print the a matrices
     std::cout << "A Matrix:" << std::endl;
-    for (int i = 0; i < N; ++i)
-    {
-        for (int j = 0; j < N; ++j)
-        {
-            std::cout << h_A[i * N + j] << "\t";
-        }
-        std::cout << std::endl;
-    }
-    std::cout << std::endl;
-    // Print the b matrix
+    //matrixPrint(h_A, N);
     std::cout << "B Matrix:" << std::endl;
-    for (int i = 0; i < N; ++i)
-    {
-        for (int j = 0; j < N; ++j)
-        {
-            std::cout << h_B[i * N + j] << "\t";
-        }
-        std::cout << std::endl;
-    }
-    std::cout << std::endl;
-    // Print the result matrix
+    //matrixPrint(h_B, N);
     std::cout << "Result Matrix:" << std::endl;
-    for (int i = 0; i < N; ++i)
-    {
-        for (int j = 0; j < N; ++j)
-        {
-            std::cout << h_C[i * N + j] << "\t";
-        }
-        std::cout << std::endl;
-    }
+    //matrixPrint(h_C, N);
 
     // Free allocated memory
     delete[] h_A;
