9a47588ce1d9508b6caa7abe2ad2b53c857ecc91
Julio <ulitoo@gmail.com>
2024-07-08 16:02:31 -0700

    epsilon

--- Basics/BLAS/2.PrecisionExample.cpp
+++ Basics/BLAS/2.PrecisionExample.cpp
@@ -10,7 +10,7 @@ using namespace std;
 // long double
 // double
 // float
-
+/*
 int main()
 {
     long double one=1.;
@@ -28,6 +28,27 @@ int main()
     }
     
     std::cout << "Iter: " << j-1 << " Value:" << pow(half,static_cast<long double>(j-1)) << endl;
+
+    return 0;
+}
+*/
+int main()
+{
+     double one=1.;
+     double half=1./2.;
+     double tmp;
+    int j;
+
+    for (j=0;;j++){
+        tmp = 1.+pow(half,static_cast< double>(j)); 
+        
+        if ((tmp-one) == 0.){
+            break;
+        } 
+
+    }
+    
+    std::cout << "Iter: " << j-1 << " Value:" << pow(half,static_cast< double>(j-1)) << endl;
 
     return 0;
 }
